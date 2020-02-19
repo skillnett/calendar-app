4f351fd780a05929783bce6bcddd51f05e0e7738
rnemyrka <rnemyrka@byteant.com>
2020-02-19 23:20:40 +0200

    Updates

---
+++ precache-manifest.56090933828d85c03bbac6ee3e464a75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba89c91f440255109c5657da2b5ef68f",
+    "url": "/calendar-app/index.html"
+  },
+  {
+    "revision": "73642f8d05bc8b9a5d03",
+    "url": "/calendar-app/static/css/2.c4058030.chunk.css"
+  },
+  {
+    "revision": "b003938593e2e4197dc4",
+    "url": "/calendar-app/static/css/main.28c32b4e.chunk.css"
+  },
+  {
+    "revision": "73642f8d05bc8b9a5d03",
+    "url": "/calendar-app/static/js/2.6355e18f.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/calendar-app/static/js/2.6355e18f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b003938593e2e4197dc4",
+    "url": "/calendar-app/static/js/main.9bc531c6.chunk.js"
+  },
+  {
+    "revision": "aa8930072b7decdd3b94",
+    "url": "/calendar-app/static/js/runtime-main.102822ef.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.14345e98600fed67df5274d80cf93f55.js"
+  "/calendar-app/precache-manifest.56090933828d85c03bbac6ee3e464a75.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/calendar-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
